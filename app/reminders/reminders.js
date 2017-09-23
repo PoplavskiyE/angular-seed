b2d391b35916de4bc99be1e4de99af19494df37b
Evgeny Poplavskiy <poplavskiy.ei@gmail.com>
2017-09-23 15:13:57 +0300

    Added saving reminders to localStorage.

--- app/reminders/reminders.js
+++ app/reminders/reminders.js
@@ -3,40 +3,64 @@ var options = {
     year: 'numeric',
     month: 'numeric',
     day: 'numeric',
-  };
-var model = {
-    items: [
-        { reminder: "Купить хлеб", description: "Черный. <100руб." ,due_date : new Date().toLocaleString("en-US", options)},
-        { reminder: "Заменить масло", description: "Позвонить 29 7071515, 10руб" ,due_date : new Date().toLocaleString("en-US", options)},
-        { reminder: "купиь билеты в кино", description: "afisha.tut.by" ,due_date : new Date().toLocaleString("en-US", options)},
-    ]
 };
+
 var remindersApp = angular.module('myApp.reminders', ['ngRoute']);
 
-remindersApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/reminders', {
-    templateUrl: 'reminders/reminders.html',
-    controller: 'RemindersCtrl'
-  });
+remindersApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/reminders', {
+        templateUrl: 'reminders/reminders.html',
+        controller: 'RemindersCtrl'
+    });
 }]);
 
-remindersApp.controller('RemindersCtrl', function($scope) {
-    $scope.list = model;
+remindersApp.controller('RemindersCtrl', function ($scope) {
+    var localSet = function () {
+        localStorage.setItem('items', JSON.stringify($scope.items));
+    }
+    var setMinDate = function () {
+        var date = new Date();
+        var day = date.getDate();
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear();
+
+        if (month < 10) month = "0" + month;
+        if (day < 10) day = "0" + day;
+
+        var today = year + "-" + month + "-" + day;
+        document.getElementById("theDate").min = today;
+    }
+    setMinDate();
+
+    $scope.saved = localStorage.getItem('items');
+    $scope.items = ($scope.saved != null) ? JSON.parse($scope.saved) : [];
+
     $scope.addItem = function (title, description, due_date) {
-        if(title == null || title == ""){
+        if (title == null || title == "") {
             alert("Please Fill Title Field");
             return;
         }
-        if(description==null || description == ""){
-            alert("Please Fill Description Field");            
+        if (description == null || description == "") {
+            alert("Please Fill Description Field");
             return;
         }
-        if(due_date==null || due_date == ""){
-            alert("Please, set due date");            
+        if (due_date == null || due_date == "") {
+            alert("Please, set due date");
             return;
         }
-        
-        var d =due_date.toLocaleString("en-US", options);
-        $scope.list.items.push({ reminder: title, description: description,due_date : d});
+
+        var d = due_date.toLocaleString("en-US", options);
+
+        $scope.items.push({
+            reminder: title,
+            description: description,
+            due_date: d
+        });
+
+        // clear input field after saving
+        $scope.title = '';
+        $scope.description = '';
+        $scope.due_date = '';
+        localSet();
     }
 });
